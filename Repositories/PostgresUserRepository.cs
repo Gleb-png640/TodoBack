a5d47be5bbc6573985f2aa02fba2958bda95d952
Глеб <@DESKTOP-UK1ALM1>
2026-03-06 20:45:13 +0300

    Added refresh token and refreshing

--- Repositories/PostgresUserRepository.cs
+++ Repositories/PostgresUserRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using TodoBack.Data;
 using TodoBack.Dtos.Users;
 using TodoBack.Models.Users;
@@ -22,11 +21,18 @@ namespace TodoBack.Repositories {
 
         public User? GetByEmail(string email) {
             return _db.Users
-                .AsNoTracking()
                 .FirstOrDefault(u => u.Email == email);
         }
 
-        public string? Login(LoginUserDto dto, IPasswordHasher<User> passwordHasher, JwtTokenServices jwt) {
+
+        private User? FindById(Guid userId)
+        {
+            return _db.Users
+                .Where(u => u.Id == userId)
+                .FirstOrDefault();
+        }
+
+        public TokenResponeDto? Login(LoginUserDto dto, IPasswordHasher<User> passwordHasher, JwtTokenServices jwt) {
 
             // Searching by email in DB
             var user = GetByEmail(dto.Email);
@@ -36,9 +42,43 @@ namespace TodoBack.Repositories {
             var passwordVerificationResult = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password);
             if (passwordVerificationResult == PasswordVerificationResult.Failed) { return null; }
 
-            string token = jwt.CreateJWT(user);
+            TokenResponeDto response = CreateTokenResponse(jwt, user);
 
-            return token;
+            _db.SaveChanges();
+
+            return response;
+        }
+
+
+        public TokenResponeDto? RefreshTokens(RefreshTokenRequestDto dto, JwtTokenServices jwt)
+        {
+            var user = ValidateRefreshToken(dto.UserId, dto.RefreshToken);
+
+            if (user is null) { return null; }
+
+            TokenResponeDto response = CreateTokenResponse(jwt, user);
+
+            return response;
+        }
+
+        private User? ValidateRefreshToken(Guid userId, string token) 
+        {
+            var user = FindById(userId);
+
+            if (user is null || user.RefreshToken != token || user.RefreshTokenExpiryTime <= DateTime.UtcNow) { return null; }
+
+            return (user);
+        }
+
+
+        private TokenResponeDto CreateTokenResponse(JwtTokenServices jwt, User user)
+        {
+            TokenResponeDto response = jwt.CreateJWT(user);
+            user.RefreshToken = response.RefreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(10);
+
+            _db.SaveChanges();
+            return response;
         }
     }
 }
--- Services/Security/JwtTokenServices.cs
+++ Services/Security/JwtTokenServices.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using TodoBack.Dtos.Users;
 using TodoBack.Models.Users;
 
 namespace TodoBack.Services.Security
@@ -9,7 +11,7 @@ namespace TodoBack.Services.Security
     public class JwtTokenServices(IConfiguration configuration)
     {
 
-        public string CreateJWT(User user)
+        public TokenResponeDto CreateJWT(User user)
         {
 
             List<Claim> claims = new()
@@ -23,14 +25,28 @@ namespace TodoBack.Services.Security
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
 
             var tokenDescriptor = new JwtSecurityToken(
-                    issuer: configuration.GetValue<string>("Jwt:Issuer"),
-                    audience: configuration.GetValue<string>("Jwt:Audience"),
-                    claims: claims,
-                    expires: DateTime.UtcNow.AddDays(1),
-                    signingCredentials: creds
+                issuer: configuration.GetValue<string>("Jwt:Issuer"),
+                audience: configuration.GetValue<string>("Jwt:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: creds
             );
 
-            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+            var response = new TokenResponeDto
+            {
+                AccesToken = new JwtSecurityTokenHandler().WriteToken(tokenDescriptor),
+                RefreshToken = GenerateRefreshToken()
+            };
+
+            return response;
+        }
+
+        private string GenerateRefreshToken() 
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
     }
 }
