248820c0aa67d463f0b2380e85e76617f4caf832
Gleb-png640 <savkinanrey@gmail.com>
2026-03-14 20:18:28 +0300

    Started to implement PostgresTasksRepo using Dapper

---
+++ Repositories/PostgresTaskRepositoryDapper.cs
@@ -0,0 +1,104 @@
+﻿using Dapper;
+using Npgsql;
+using System.Data;
+using TodoBack.Dtos.Tasks;
+using TodoBack.Models.Tasks;
+using TodoBack.QueryParameters;
+
+namespace TodoBack.Repositories
+{
+    public class PostgresTaskRepositoryDapper : ITaskRepository
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public PostgresTaskRepositoryDapper(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+
+        public Task<UserTask> AddAsync(UserTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserTask> ChangeExistingTaskAsync(UserTask task, UpdateUserTaskDto taskDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(UserTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserTask?> GetByIdAsync(int id, Guid UserId)
+        {
+            using (var connection = GetConnection()) 
+            {
+                const string sql =
+                    """
+                    SELECT * 
+                    FROM "Tasks"
+                    WHERE "UserId" = @UserId AND "TaskId" = @id
+                    """;
+
+                return await connection.QueryFirstOrDefaultAsync<UserTask?>(sql, new 
+                    {
+                        UserId = UserId,
+                        id = id
+                    });
+            }
+        }
+
+        public async Task<UserTask?> GetByIdTrackedAsync(int id, Guid UserId)
+        {
+            using (var connection = GetConnection())
+            {
+                const string sql =
+                    """
+                    SELECT * 
+                    FROM "Tasks"
+                    WHERE "UserId" = @UserId AND "TaskId" = @id
+                    """;
+
+                return await connection.QueryFirstOrDefaultAsync<UserTask?>(sql, new
+                {
+                    UserId = UserId,
+                    id = id
+                });
+            }
+        }
+
+        public async Task<IEnumerable<UserTask>> GetPagedAsync(GetPageQuery query, Guid UserId)
+        {
+            using var connection = GetConnection();
+
+            const string sql =
+                """
+                    SELECT  *
+                    FROM "Tasks"
+                    WHERE "UserId" = @UserId
+                    ORDER BY "TaskId"
+                    LIMIT @PageSize OFFSET @Offset
+                """;
+
+            return await connection.QueryAsync<UserTask>(sql, new 
+            { 
+                UserId = UserId,
+                PageSize = query.pageSize,
+                Offset = query.pageSize * (query.page - 1)
+            });
+
+        }
+
+
+        private IDbConnection GetConnection()
+        {
+            var connString = _configuration.GetConnectionString("DefaultConnection");
+
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
